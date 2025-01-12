8b743d1970e8e8b9ea17f05e3bad2f310946d03e
jb <jbnouse@gmail.com>
2025-01-12 08:31:21 -0500

    refactoring

--- app.py
+++ app.py
@@ -10,34 +10,30 @@ import numpy as np
 import base64
 from model import is_first_player, upload_image_to_db, delete_previous_round, fetch_images_from_db
 
-# Page icon
+# Initial Page Configurations
 icon = Image.open('assets/logo-small.png')
-
 st.set_page_config(
         page_title="TuringSketch",
         page_icon=icon
     )
 
-page_bg_img = """
-    <style>
-    background-color: #f7f7f7;
-    </style>
-    """
-st.markdown(page_bg_img, unsafe_allow_html=True)
-# Set page title
+
 def play():
+    """
+    Transition to the play screen.
+    """
     # Update the screen state to "play"
     st.session_state.screen = "play"
     st.session_state.prompt = random.choice(prompts)
+
+    # If the player is the first player, delete the previous round and upload the new prompt
     if is_first_player() == True:
         delete_previous_round()
         upload_image_to_db(st.session_state.prompt, None)
         if "start_time" not in st.session_state:
             
             # Loading gif
-            # Create nine columns
             col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(9)
-            # Add widgets to each column
             with col1:
                 pass
             with col2:
@@ -67,11 +63,8 @@ def play():
 
             st.markdown("<p style='text-align: center;'>Searching for players...</p>", unsafe_allow_html=True)
             st.divider()
-            while True:
-                time.sleep(1)
-                if is_first_player() == True:
-                    st.session_state.start_timer = True
-                    break
+
+    # If the player is the second player, fetch the images from the database
     else:
         images = fetch_images_from_db()
         otherPrompt = images[0].prompt
@@ -79,16 +72,24 @@ def play():
         while otherPrompt == st.session_state.prompt:
             st.session_state.prompt = random.choice(prompts)
         upload_image_to_db(st.session_state.prompt, None)
-        st.session_state.start_timer = True
+
 
 def main():
+    ###################################################
+    #                     HEADER                      #
+    ###################################################
     logo = Image.open("assets/logo.png")
     st.image(logo)
 
+
+    ###################################################
+    #                   HOME SCREEN                   #
+    ###################################################
     # Initialize the screen in session state
     if "screen" not in st.session_state:
         st.session_state.screen = "home"
 
+    # Rules and Play button
     if st.session_state.screen == "home":
         st.markdown("<p style='text-align: center;'>Welcome to TuringSketch! The game where you have to guess which drawing was made by AI.</p>", unsafe_allow_html=True)
         st.markdown("<h1 style='text-align: center; color: red;'>Rules</h1>", unsafe_allow_html=True)
@@ -96,11 +97,9 @@ def main():
                 "<p style='text-align: center;'>You will be given a prompt to draw.</p>"
                 "<p style='text-align: center;'>After drawing, you will be shown two images: one drawn by AI and one drawn by a human.</p>"
                 "<p style='text-align: center;'>Your task is to guess which image was drawn by AI.</p>", unsafe_allow_html=True)
-        
 
         # Create seven columns
         col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
-        # Add widgets to each column
         with col1:
             pass
         with col2:
@@ -108,13 +107,7 @@ def main():
         with col3:
             pass
         with col4:
-            if 'button_clicked' not in st.session_state:
-                st.session_state.button_clicked = False
-
-                st.button(label="Play", on_click=play, disabled=st.session_state.button_clicked)
-                st.session_state.button_clicked = True
-            else:
-                st.write('Button is disabled.')
+            st.button(label="Play", on_click=play)
         with col5:
             pass
         with col6:
@@ -122,24 +115,23 @@ def main():
         with col7:
             pass
 
-    elif st.session_state.screen == "play":
 
+    ###################################################
+    #                   PLAY SCREEN                   #
+    ###################################################
+    elif st.session_state.screen == "play":
         st.header("Prompt: " + st.session_state.prompt)
         
-        # Create three columns
+        # Drawing Tools
         col1, col2, col3 = st.columns(3)
-
-        # Add widgets to each column
         with col1:
             stroke_width = st.slider("Stroke width", 1, 25, 3)
-
         with col2:
             stroke_color = st.color_picker("Stroke color", "#000000")
-
         with col3:
             bg_color = st.color_picker("Background color", "#FFFFFF")
 
-        # Create the canvas
+        # Drawing Canvas
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
             stroke_width=stroke_width,
@@ -151,21 +143,30 @@ def main():
             key="canvas",
         )
 
-        def to_blob(img):
-            """Converts the dataclass instance to a BLOB."""
+        def to_blob(img:np.ndarray) -> bytes | None:
+            """
+            Converts the dataclass instance to a BLOB.
+            """
             image_data_blob = img.tobytes() if img is not None else None
             return image_data_blob
 
-        def done():
+        def guess() -> None:
+            """
+            Transition to the guess screen and upload the image to the database.
+            """
             st.session_state.screen = "guess"
             blob = to_blob(canvas_result.image_data)
             upload_image_to_db(st.session_state.prompt, blob)
             
-        st.button(label="Done", on_click=done)
+        st.button(label="Done", on_click=guess)
 
-        url = f"https://pollinations.ai/p/A very poorly drawn black and white image of a {st.session_state.prompt}, created in under 30 seconds with minimal detail. The drawing should use rough, uneven lines, simple shapes, and only basic colors. It should look like it was drawn hurriedly in a basic MSpaint, with no shading or intricate features. The background is plain white.?width=512&height=512"
+        url = f"https://pollinations.ai/p/A poorly drawn black and white line art of a {st.session_state.prompt}, created in under 30 seconds by a 3 year old child. The drawing should use lines that are solid, wiggly, and hex code #000000. It should look like it was drawn in MSpaint, with no shading and no details. Only use simple shapes. The background color should be #FFFFFF. All lines should have the same width.?width=512&height=512"
         st.session_state.img = requests.get(url)
         
+
+    ###################################################
+    #                  GUESS SCREEN                   #
+    ###################################################
     elif st.session_state.screen == "guess":
         st.title("Which picture is made by AI?")
         
@@ -206,7 +207,7 @@ def main():
             else:
                 st.image(human_image)
             st.button(label="This is AI.", on_click=lambda: finish("right", ground_truth), key="right")
-
+            
         def finish(answer, ground_truth):
             """
             Set the screen state to "finish" when the user clicks the button.
@@ -216,6 +217,10 @@ def main():
             st.session_state['answer'] = answer
             st.session_state['ground_truth'] = ground_truth
 
+
+    ###################################################
+    #                 FINISH SCREEN                   #
+    ###################################################
     elif st.session_state.screen == "finish":
         ground_truth = st.session_state['ground_truth']
         answer = st.session_state['answer']
